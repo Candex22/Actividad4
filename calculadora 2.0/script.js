e8786e08e4ca509ff58401f3bc7fe8c76bd61eda
Candex22 <candemolinari20@gmail.com>
2025-04-09 21:03:16 -0300

    age ajusment

--- calculadora 2.0/script.js
+++ calculadora 2.0/script.js
@@ -1,16 +1,31 @@
 let display = document.getElementById("display");
+let resultDisplay = document.getElementById("result-display");
 
-let buttonSound = new Audio("sounds/boton.mp3");
+let buttonSound = new Audio("sounds/quack.mp3");
 let errorSound = new Audio("sounds/error.mp3");
+let bubbleSound = new Audio("sounds/bubble.mp3");
+let cuack = new Audio("sounds/cuack.mp3");
+let duckRainSound = new Audio("sounds/duck_rain.mp3"); // Sonido para la lluvia de patitos
+let duckTalkSound = new Audio("sounds/duck_talk.mp3"); // Sonido para el pato que habla
 
 function playButtonSound() {
-    buttonSound.currentTime = 0; 
+    buttonSound.currentTime = 0;
     buttonSound.play();
 }
 
 function playErrorSound() {
     errorSound.currentTime = 0;
     errorSound.play();
+}
+
+function scuack() {
+    cuack.currentTime = 0;
+    cuack.play();
+}
+
+function playDuckRainSound() {
+    duckRainSound.currentTime = 0;
+    duckRainSound.play();
 }
 
 function appendValue(value) {
@@ -28,11 +43,43 @@ function deleteLast() {
     display.value = display.value.slice(0, -1);
 }
 
+function safeCalculate(expression) {
+    try {
+        return new Function('return ' + expression)();
+    } catch (error) {
+        return NaN;
+    }
+}
+
+function showDuckRain() {
+    let rain = "";
+    let centerX = window.innerWidth / 2;
+    let centerY = window.innerHeight / 2;
+
+    for (let i = 0; i < 30; i++) {
+        let angle = Math.random() < 0.5 ? 0 : Math.PI;
+        let distance = Math.random() * (window.innerWidth / 2);
+
+        let x = Math.cos(angle) * distance;
+        let y = (Math.random() * 100 - 50);
+
+        rain += `<img src='img/pato.png' alt='Pato' class='duck-confetti' style='width: 20px; left: ${centerX + x}px; top: ${centerY + y}px; --x: ${x / distance}; --y: ${y / distance};'>`;
+    }
+    resultDisplay.innerHTML += rain;
+
+    setTimeout(() => {
+        let confetti = document.querySelectorAll('.duck-confetti');
+        confetti.forEach(duck => duck.remove());
+    }, 1500);
+}
+
 function calculateResult() {
     try {
         let result = eval(display.value);
         if (isNaN(result) || result === Infinity || result === -Infinity) {
             throw new Error();
+        } else {
+            showDuckRain();
         }
         display.value = result;
     } catch {
@@ -56,4 +103,3 @@ document.addEventListener("keydown", function(event) {
         clearDisplay();
     }
 });
-
